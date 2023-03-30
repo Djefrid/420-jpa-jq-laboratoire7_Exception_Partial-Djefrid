c1332651c71354935fb2bf3814d5a2d7cb3bc66f
Djefrid Byli Fotue Kuate <2210038@etu.cegepjonquiere.ca>
2023-03-30 14:19:08 -0400

    parti 1

---
+++ Partie1/Calculateur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partie1
+{
+    internal class Calculateur
+    {
+
+        public Calculateur() { }
+
+
+        public double Diviser(int nbre1, int nbre2) 
+        {
+            int rslt = 0;
+
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rslt;
+        }
+    }
+}
